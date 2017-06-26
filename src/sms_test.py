ab7dbe823a092b04867446f64b67afdf3ac9fc24
moyuliang <huangewo@gmail.com>
2017-06-26 09:56:28 +0800

    change path

--- src/sms_test.py
+++ src/sms_test.py
@@ -1,15 +1,15 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC0fb924403cb65b49c70ec2b0e60aceca"
+account_sid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxca"
 # Your Auth Token from twilio.com/console
-auth_token  = "f0159e724e0a30552e6637fd87639615"
+auth_token  = "f0xxxxxxxxxxxxxxxxxxxxxxxxxxxx15"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+18786111008",# Your Phone Number
-    from_="+12568294346",# Your Twilio Number
+    to="+86xxxxxxxxxxx",# Your Phone Number
+    from_="+xxxxxxxxxxx",# Your Twilio Number
     body="Hello from Python!")
 
 print(message.sid)
