1f1309a41e7412a8e7b683e47fcaeb6a6de280c4
moyuliang <huangewo@gmail.com>
2018-05-04 17:06:12 +0800

    python

--- src/lesson0/more_python.py
+++ src/lesson0/more_python.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import math
 from functools import reduce
+import random
 
 # 高阶函数：将函数作为参数
 
@@ -15,71 +16,35 @@ print "add(25, 9, math.sqrt)=%s" % add(25, 9, math.sqrt)
 # 并通过把函数 f 依次作用在 list 的每个元素上，得到一个新的 list 并返回。
 # 输出[1, 4, 9, 10, 25, 36, 49, 64, 81]
 
-
-def f(x):
-    return x * x
-
-
-print "map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])=%s" % map(
-    f, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+print "map(lambda x:x*x, [1, 2, 3, 4, 5, 6, 7, 8, 9])=%s" % map(
+    lambda x: x * x, [1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 # 字符串首字母大写
 # 输出['Adam', 'Lisa', 'Bart']
 
-
-def format_name(s):
-    return s.capitalize()
-
-
-print "map(format_name, ['adam', 'LISA', 'barT'])=%s" % map(
-    format_name, ['adam', 'LISA', 'barT'])
+print "map(lambda s:s.capitalize(), ['adam', 'LISA', 'barT'])=%s" % map(
+    lambda s: s.capitalize(), ['adam', 'LISA', 'barT'])
 
 # reduce()传入的函数 f 必须接收两个参数，reduce()对list的每个元素反复调用函数f，并返回最终结果值。
 # reduce()还可以接收第3个可选参数，作为计算的初始值。
 # 2*(2*4*5*7*12)
 
-
-def prod(x, y):
-    return x * y
-
-
-print "reduce(prod, [2, 4, 5, 7, 12],2)=%s" % reduce(prod, [2, 4, 5, 7, 12], 2)
+print "reduce(lambda x,y:x*y, [2, 4, 5, 7, 12],2)=%s" % reduce(lambda x, y: x * y, [2, 4, 5, 7, 12], 2)
 
 # s.strip(rm) 删除 s 字符串中开头、结尾处的 rm 序列的字符。
 # 当rm为空时，默认删除空白符（包括'\n', '\r', '\t', ' ')
 
-
-def is_not_empty(s):
-    return s and len(s.strip()) > 0
-
-
-print filter(is_not_empty, ['test', None, '',
-                            'str', '  ', 'END', '\t\t123\r\n'])
+print filter(lambda s: s and len(s.strip()) > 0, ['test', None, '',
+                                                  'str', '  ', 'END', '\t\t123\r\n'])
 
 # 利用filter()过滤出1~100中平方根是整数的数
+# 小数点后面为0时，对1取余才为0
 
+print filter(lambda x: math.sqrt(x) % 1 == 0, range(1, 101))
 
-def is_sqr(x):
-    # 开根后变成浮点数，所以无法用是否为整数判断。可以平方后与元数值相等判断
-    # if math.sqrt(x)**2==x:
-    #     return x
-    return math.sqrt(x) % 1 == 0  # 小数点后面为0时，对1取余才为0
+# 忽略大小写排序
 
-
-print filter(is_sqr, range(1, 101))
-
-#忽略大小写排序
-def cmp_ignore_case(s1, s2):
-    # if s1.lower()>s2.lower():
-    #     return 1
-    # if s1.lower()<s2.lower():
-    #     return -1
-    # if s1.lower()==s2.lower():
-    #     return 0
-    return cmp(s1.lower(),s2.lower())
-
-print sorted(['bob', 'about', 'Zoo', 'Credit'], cmp_ignore_case)
-
+print sorted(['bob', 'about', 'Zoo', 'Credit'], lambda s1, s2: cmp(s1.lower(), s2.lower()))
 
 
 # 内层函数f()引用了外层函数calc_prod()的变量lst（参数也算变量），然后返回内层函数的情况，称为闭包（Closure）
@@ -88,30 +53,90 @@ print sorted(['bob', 'about', 'Zoo', 'Credit'], cmp_ignore_case)
 def calc_prod(lst):
     def a(x, y):
         return x * y
+
     def f():
         return reduce(a, lst)
     return f
 
 
-f = calc_prod([1, 2, 3, 4])
-print f
-print f()
+# f = calc_prod([1, 2, 3, 4])
+# print f
+# print f()
 
 # 当count()函数返回了3个函数时，这3个函数所引用的变量 i 的值已经变成了3。由于f1、f2、f3并没有被调用，所以，此时他们并未计算 i*i
 # 因此，返回函数不要引用任何循环变量，或者后续会发生变化的变量。
+
+
 def count():
     fs = []
     for i in range(1, 4):
-    #     def f():
-    #          return i*i
-    #     fs.append(f)
+        #     def f():
+        #          return i*i
+        #     fs.append(f)
         def f(m=i):
             return m * m
         fs.append(f)
 
     return fs
-f1, f2, f3 = count()
-print f1(),f2(),f3()
+
+#
+# f1, f2, f3 = count()
+# print f1(), f2(), f3()
+
+# 分数计算
 
 
-print filter(lambda s:s and len(s.strip()) > 0, ['test', None, '', 'str', '  ', 'END'])
+class Rational(object):
+    def __init__(self, p, q):
+        self.p = p
+        self.q = q
+
+    def __add__(self, r):
+        return Rational(self.p * r.q + self.q * r.p, self.q * r.q)
+
+    def __sub__(self, r):
+        return Rational(self.p * r.q - self.q * r.p, self.q * r.q)
+
+    def __mul__(self, r):
+        return Rational(self.p * r.p, self.q * r.q)
+
+    def __div__(self, r):
+        return Rational(self.p * r.q, self.q * r.p)
+
+    def __str__(self):
+        if self.p > self.q:
+            smaller = self.q
+        else:
+            smaller = self.p
+        for i in range(1, smaller + 1):
+            if ((self.p % i == 0) and (self.q % i == 0)):
+                gys = i
+        return '%s/%s' % (self.p / gys, self.q / gys)
+
+    __repr__ = __str__
+
+
+# r1 = Rational(1, 2)
+# r2 = Rational(1, 4)
+# print r1 + r2
+# print r1 - r2
+# print r1 * r2
+# print r1 / r2
+
+# 斐波那契数列
+
+
+class Fib(object):
+    # 把类变成跟函数一样的可调用对象
+    def __call__(self, n):
+        a, b = 0, 1
+        L = []
+        while n > 0:
+            a, b = b, a + b
+            L.append(a)
+            n -= 1
+        return L
+
+
+# f = Fib()
+# print f(10)
--- src/project/text.py
+++ src/project/text.py
@@ -1,40 +1,78 @@
 # -*- coding: utf-8 -*-
 
 # 逆转字符串——输入一个字符串，将其逆转并输出。
+import random
+
+
 def reverse_string(a_str):
     return a_str[::-1]
 
 # 拉丁猪文字游戏——这是一个英语语言游戏。
 # 基本规则是将一个英语单词的第一个辅音音素的字母移动到词尾并且加上后缀-ay（譬如“banana”会变成“anana-bay”）
+
+
 def ladin_pig(a_str):
-    for i,value in enumerate(a_str):
-        if value not in ('a','e','i','o','u'):
-            return a_str[:i]+a_str[i+1:]+"-"+a_str[i]+"ay"
+    for i, value in enumerate(a_str):
+        if value not in ('a', 'e', 'i', 'o', 'u'):
+            return a_str[:i] + a_str[i + 1:] + "-" + a_str[i] + "ay"
     return False
 
 # 统计元音字母——输入一个字符串，统计处其中元音字母的数量。更复杂点的话统计出每个元音字母的数量。
+
+
 def vowel_count(a_str):
-    dic={'a':0,'e':0,'i':0,'o':0,'u':0}
+    dic = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
 
     for a in a_str.lower():
-        for key,value in dic.items():
-            if a==key:
-                dic[key]=dic[key]+1
+        for key, value in dic.items():
+            if a == key:
+                dic[key] = dic[key] + 1
     return dic
 
 # 判断是否为回文——判断用户输入的字符串是否为回文。回文是指正反拼写形式都是一样的词，譬如“racecar”。
+
+
 def is_palindrome(a_str):
-    if a_str==a_str[::-1]:
+    if a_str == a_str[::-1]:
         return True
     return False
 
-case = int(raw_input("Please enter a num:"))
+# 一个猜数字的游戏，随机生成一个数字，提示用户输入的数字与生成数字比较大小的结果，知道用户猜中生成数字为止
 
-if case==1:
-    print reverse_string(raw_input("Please enter a string:"))
-if case==2:
-    print ladin_pig(raw_input("Please enter a word:"))
-if case==3:
-    print vowel_count(raw_input("Please enter a string:"))
-if case==4:
-    print is_palindrome(raw_input("Please enter a string:"))
+
+def random_game():
+    num = random.randint(0, 100)
+
+    while True:
+        try:
+            guess = int(raw_input("Please enter a number in 1~100:"))
+        except ValueError, e:
+            print "Input Error\n"
+            continue
+
+        if guess > num:
+            print "Your number is bigger than right number:", guess
+        elif guess < num:
+            print "Your number is smaller than right number:", guess
+        else:
+            print "Guess right,Game Over!"
+            break
+        print "\n"
+
+
+while True:
+    case = int(raw_input("1.逆转字符串\t2.拉丁猪文字游戏\t3.统计元音字母\t\t4.判断回文\t5.猜数字\t0.退出\nPlease enter a num:"))
+
+    if case == 1:
+        print reverse_string(raw_input("Please enter a string:"))
+    if case == 2:
+        print ladin_pig(raw_input("Please enter a word:"))
+    if case == 3:
+        print vowel_count(raw_input("Please enter a string:"))
+    if case == 4:
+        print is_palindrome(raw_input("Please enter a string:"))
+    if case == 5:
+        random_game()
+    if case == 0:
+        print "已退出游戏"
+        break
