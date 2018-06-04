6d1d8118f26180c5fc38485653cc6080f106eca5
moyuliang <huangewo@gmail.com>
2018-06-04 16:51:07 +0800

    爬取百度百科python页面

--- src/baike_spider/html_parser.py
+++ src/baike_spider/html_parser.py
@@ -17,7 +17,7 @@ class HtmlParser(object):
 
     def _get_new_urls(self, page_url, soup):
         new_urls = set()
-        links = soup.find_all('a',href=re.compile(r"/view/\d+\.htm"))
+        links = soup.find_all('a',href=re.compile(r"/item/\d+\.htm"))
         for link in links:
             new_url=link['href']
             new_urls.add("https://baike.baidu.com"+new_url)
